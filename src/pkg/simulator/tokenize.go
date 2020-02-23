d7fa8c95b497db147f4dba977c76bd45b83f519d
Damon Douglas <damon.douglas@ditoweb.com>
2020-02-22 21:47:15 -0800

    documentation

--- src/pkg/simulator/tokenize.go
+++ src/pkg/simulator/tokenize.go
@@ -9,6 +9,10 @@ var (
 	tokenColumns = []int{1,2,4}
 )
 
+// Tokenize a beam.PCollection expected as
+// dbid    drug    known   misspellingPhon edOne   edTwo   pillMark        google_ms       google_title    google_snippet  ud_slang        missed
+// See https://zenodo.org/record/3238718#.Xk9hOpNKjOQ
+// Tab delimited columns 2, 3 and 5 are extracted and split by comma
 func Tokenize(s beam.Scope, lines beam.PCollection) beam.PCollection {
 	tabbedTokens := beam.ParDo(s, tabbedLineHandler, lines)
 	return beam.ParDo(s, commaDelimitedHandler, tabbedTokens)
